30c5da6f4a0b993b02f9a526d04d47e96f7d57a7
Travers Ching <traversc@gmail.com>
2020-06-10 06:50:03 +0000

    version 0.22.1

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -1,24 +1,27 @@
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  library(qs)
 #  df1 <- data.frame(x=rnorm(5e6), y=sample(5e6), z=sample(letters,5e6, replace=T))
 #  qsave(df1, "myfile.qs")
 #  df2 <- qread("myfile.qs")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # CRAN version
 #  install.packages("qs")
 #  
 #  # CRAN version compile from source (recommended)
 #  remotes::install_cran("qs", type="source", configure.args="--with-simd=AVX2")
+#  
+#  # For earlier versions of R <= 3.4
+#  remotes::install_github("traversc/qs@legacy")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  data.frame(a=rnorm(5e6),
 #             b=rpois(5e6,100),
 #             c=sample(starnames$IAU,5e6,T),
 #             d=sample(state.name,5e6,T),
 #             stringsAsFactors = F)
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # With byte shuffling
 #  x <- 1:1e8
 #  qsave(x, "mydat.qs", preset="custom", shuffle_control=15, algorithm="zstd")
@@ -31,28 +34,13 @@
 #  cat( "Compression Ratio: ", as.numeric(object.size(x)) / file.info("mydat.qs")$size, "\n" )
 #  # Compression Ratio:  1.479294
 
-## ----eval=FALSE----------------------------------------------------------
-#  df1 <- data.frame(x = randomStrings(1e6), y = randomStrings(1e6), stringsAsFactors = F)
-#  qsave(df1, "temp.qs")
-#  rm(df1); gc() ## remove df1 and call gc for proper benchmarking
-#  
-#  # With alt-rep
-#  system.time(qread("temp.qs", use_alt_rep=T))[1]
-#  #     0.109 seconds
-#  
-#  
-#  # Without alt-rep
-#  gc(verbose=F)
-#  system.time(qread("temp.qs", use_alt_rep=F))[1]
-#  #     1.703 seconds
-
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  library(qs)
 #  x <- qserialize(c(1,2,3))
 #  qdeserialize(x)
 #  [1] 1 2 3
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  library(qs)
 #  library(Rcpp)
 #  sourceCpp("test.cpp")
